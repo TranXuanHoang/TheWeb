d3981075d33857729ef50f3f36565e7e6c10c95c
TranXuanHoang <hoangtx.ict@gmail.com>
2020-10-11 23:44:01 +0900

    Add side navigation to packages and customers pages
    * Add the same side navigation to packages and customers pages
    * Change to different approach of toggling the side nav - by using classList instead of style.display

--- CSS/01-basics/shared.js
+++ CSS/01-basics/shared.js
@@ -7,24 +7,44 @@ const mobileNav = document.querySelector('.mobile-nav')
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener('click', () => {
-    modal.style.display = 'block'
-    backdrop.style.display = 'block'
+    // Method 1: use 'style.display'
+    // modal.style.display = 'block'
+    // backdrop.style.display = 'block'
+
+    // Method 2: use 'classList.add'
+    modal.classList.add('open')
+    backdrop.classList.add('open')
   })
 }
 
 const closeModal = () => {
-  modal.style.display = 'none'
-  backdrop.style.display = 'none'
+  // Method 1: use 'style.display'
+  // modal?.style.display = 'none'
+  // backdrop.style.display = 'none'
+
+  // Method 2: use 'classList.remove'
+  modal?.classList.remove('open')
+  backdrop.classList.remove('open')
 }
 
 backdrop.addEventListener('click', () => {
-  mobileNav.style.display = 'none'
+  // Method 1: use 'style.display'
+  // mobileNav.style.display = 'none'
+
+  // Method 2: use 'classList.remove'
+  mobileNav.classList.remove('open')
+
   closeModal()
 })
 
-modalNoButton.addEventListener('click', closeModal)
+modalNoButton?.addEventListener('click', closeModal)
 
 toggleButton.addEventListener('click', () => {
-  mobileNav.style.display = 'block'
-  backdrop.style.display = 'block'
+  // Method 1: use 'style.display'
+  // mobileNav.style.display = 'block'
+  // backdrop.style.display = 'block'
+
+  // Method 2: use 'classList.add'
+  mobileNav.classList.add('open')
+  backdrop.classList.add('open')
 })
